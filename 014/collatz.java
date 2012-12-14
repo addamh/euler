8bee7931221ad3c37590648f11b6bd1201f22041
Addam Hardy <addam.hardy@gmail.com>
2012-12-14 12:29:18 -0600

    Adding java solution to #14

---
+++ 014/collatz.java
@@ -0,0 +1,30 @@
+//&>/dev/null;x="${0%.*}";[ ! "$x" -ot "$0" ]||javac $0 && java -classpath ${0%/*} ${x##*/}
+
+public class collatz {
+  public static void main(String[] args) {
+    int p;
+    long a = 0;
+    long b = 0;
+    long c;
+       
+    for (int i =0;i<1000000;i++){
+      p=1;
+      c=i;
+           
+      while(c>1){
+        if(c%2==0){
+          c=c/2;
+          p++;  
+        } else {
+          c=3*c+1;
+          p++; 
+        }
+      }
+      if(p>a){
+        a=p;
+        b=i;
+      }
+    }
+    System.out.println(a+" -> "+b);
+  }
+}
